86b0164b415e7b2aa1f4cf14a555db8146d80267
ArtN0nymous <jr616934@gmail.com>
2023-03-27 09:49:29 -0700

    compartir

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../../interfaces/index';
 import { Browser } from '@capacitor/browser';
+import { ActionSheetController, Platform } from '@ionic/angular';
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 
 @Component({
   selector: 'app-article',
@@ -9,14 +11,48 @@ import { Browser } from '@capacitor/browser';
 })
 export class ArticleComponent {
 
-  constructor() { }
+  constructor(private actionSheetCtrl:ActionSheetController,private socialSharing:SocialSharing,private platform:Platform) { }
 
   @Input() i!:number;
   @Input() item!:Article;
   async openArticle(){
     await Browser.open({ url: this.item.url });
   }
-  onClick(){
-
+  async onOpenMenu(){
+    let buttons=[
+      {
+        text:'Favorito',
+        icon:'heart-outline',
+        handler:()=>this.onToggleFavorite()
+      },
+      {
+        text:'Cancelar',
+        icon:'close-outline',
+        role:'cancel'
+      }
+    ]
+    if(this.platform.is('capacitor')){
+      buttons.unshift({
+        text:'Compartir',
+        icon:'share-outline',
+        handler:()=>this.onShare()
+      },)
+    }
+    const actionShett=await this.actionSheetCtrl.create({
+      header:'Opciones',
+      buttons:buttons
+    });
+    await actionShett.present();
+  }
+  onShare(){
+    this.socialSharing.share(
+      this.item.title,
+      this.item.source.name,
+      undefined,
+      this.item.url
+    );
+  }
+  onToggleFavorite(){
+    console.log('Add favorito');
   }
 }
